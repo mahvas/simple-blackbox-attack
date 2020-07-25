fe6ef02818dae91cb0535951d0cef5dcb102838d
Gopal K. Vashishtha <gopalv@microsoft.com>
2020-07-24 17:14:38 -0700

    adding bulbul

---
+++ test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+import requests
+import numpy
+import json
+
+data = open('bulbul.jpg','rb').read()
+
+res = requests.get('stackoverflow.com')
+print(res.text)
+
+res = requests.post(url='http://817c1c6b-37b5-43a4-a947-9e252b665ead.westeurope.azurecontainer.io/score', 
+                    data=data, headers={'Content-Type':'application/octet-stream'})
+class_dict=json.loads(open('imagenet_class_index.json', 'r').read())
+
+
+out = json.loads(res.text)
+
+predicted_index=numpy.argmax(out['result'])
+
+print(f'predicted class{class_dict[str(predicted_index)]} with probability {out["result"][predicted_index]}')
